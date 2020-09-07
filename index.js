817a3409af4c9f1b6b3c168bc37fe7eba435c6c1
cpramshad <cpramshad@gmail.com>
2020-09-07 13:40:31 +0400

    [feature] adding UI for products and laptops

--- index.js
+++ index.js
@@ -9,16 +9,35 @@ const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id;
 
+    // PRODUCT OVEVIEw
     if(pathName === '/products' || pathName === '/') {
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end('This is the PRODUCTS page');
+        fs.readFile(`${__dirname}/templates/template-overview.html`,'utf-8', (err, data) => {
+            let overViewOutput = data;
+            fs.readFile(`${__dirname}/templates/template-card.html`,'utf-8', (err, data) => {
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                overViewOutput = overViewOutput.replace('{%CARDS%}', cardsOutput);
+                res.end(overViewOutput);
+            });
+        });
     }
-
+    // LAPTOP DETAILS
     else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end(`This is the LAPTOP page for laptop ${id}`);
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+            const output = replaceTemplate(data, laptop);
+            res.end(output);
+        }); 
     }
-
+    // IMAGES
+    else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+            res.writeHead(200, {'Content-type':'image/jpg'});
+            res.end(data);
+        });
+    }
+    // URL NOT FOUND
     else {
         res.writeHead(404, {'Content-type': 'text/html'});
         res.end('This URL is not found');
@@ -29,3 +48,16 @@ const server = http.createServer((req, res) => {
 server.listen(1337, '127.0.0.1', () => {
     console.log('listening for request');
 });
+
+function replaceTemplate(originalHTML, laptop) {
+    let output = originalHTML.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+}
